5ddf862ba0bd98d9b9c4701e3f3eb1b9480cc706
igorcj <cj.igor@outlook.com>
2021-03-03 15:59:16 -0300

    NLU update and Conllu merge adapted

--- app/ConlluProcess.hs
+++ app/ConlluProcess.hs
@@ -7,6 +7,7 @@ import NLU
 import Text.Regex.TDFA
 import System.Environment
 import System.Exit
+import Data.Either
 
 
 msg =
@@ -14,12 +15,12 @@ msg =
   \  test-conllu -m JSON-file CONLLU-file CoNLLU-file\n\
   \  test-conllu -c CONLLU-file  => NER and POS check to stdout\n"
 
-usage = putStrLn msg
+help = putStrLn msg
 
-parse ["-h"]    = usage >> exitSuccess
+parse ["-h"]    = help >> exitSuccess
 parse ("-m":ls) = merge ls >> exitSuccess
-parse ("-c":ls) = check ls >> exitSuccess
-parse ls        = usage >> exitFailure
+-- parse ("-c":ls) = check ls >> exitSuccess
+parse ls        = help >> exitFailure
 
 
 type Range = (Int,Int)
@@ -35,7 +36,7 @@ sentRange :: Sent -> Range -- Take tange of sentence
 sentRange s = (fst $ cwRange $ head w, snd $ cwRange $ last w) where w = _words s
 
 mentionRange :: Mention -> Range -- Rake ranges of a mention
-mentionRange (Mention t (b:e)) =  (b,head e)
+mentionRange (Mention _ [b,e] _) = (b,e)
 
 entRanges :: Entity -> [Range] -- Take ranges of entity
 entRanges e = map mentionRange $ mentions e
@@ -55,31 +56,33 @@ isMember n = foldr (\x -> (||) (n==x)) False
 cwHead :: CW AW -> ID 
 cwHead n = maybe (SID 0) _head $ _rel n
 
+jsonCheck (Left _) _ _ = putStrLn "JSON inválido"
+jsonCheck (Right js) sents outpath = writeConlluFile outpath ncl where
+  ncl = map (\s -> metaUpdate s $ filter (`entINsent` s) (entities js)) sents
+
 merge :: [FilePath] -> IO ()
-merge [pj,pc,po] = do
-  js <- readJSON pj
-  sents <- readConlluFile pc
-  let ents = entities js
-  let ncl = map (\s -> metaUpdate s $ filter (`entINsent` s) ents) sents
-  writeConlluFile po ncl
+merge [jspath,clpath,outpath] = do
+  js <- readJSON jspath
+  sents <- readConlluFile clpath
+  jsonCheck js sents outpath
 
-entCheck:: Entity -> [CW AW] -> Bool
-entCheck e l = res where
-  er = head $ entRanges e
-  nl = filter (\c -> isSubrange (cwRange c) er) l
-  nodes = map _id nl
-  roots = filter (\c -> isMember (cwHead c) nodes) nl
-  res = length roots <= 1
+-- entCheck:: Entity -> [CW AW] -> Bool
+-- entCheck e l = res where
+--   er = head $ entRanges e
+--   nl = filter (\c -> isSubrange (cwRange c) er) l
+--   nodes = map _id nl
+--   roots = filter (\c -> isMember (cwHead c) nodes) nl
+--   res = length roots <= 1
 
-sentCheck :: Sent -> [Entity]
-sentCheck s = filter (`entCheck` w) ent where
-  ent = strTOent $ snd $ last $ _meta s
-  w = _words s
+-- sentCheck :: Sent -> [Entity]
+-- sentCheck s = filter (`entCheck` w) ent where
+--   ent = strTOent $ snd $ last $ _meta s
+--   w = _words s
 
-check :: [FilePath] -> IO ()
-check (p:_) = do
-  c <- readConlluFile p
-  print $ map sentCheck c
+-- check :: [FilePath] -> IO ()
+-- check (p:_) = do
+--   c <- readConlluFile p
+--   print $ map sentCheck c
 
 main :: IO ()
 main = getArgs >>= parse
