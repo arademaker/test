bed344f0cfd63d3290b5673aaf620ee3d153b691
Wellington Silva <wellington.71319@gmail.com>
2021-03-09 16:09:30 -0300

    Remove extra structures

--- app/NEValidation.hs
+++ app/NEValidation.hs
@@ -8,7 +8,7 @@ import System.Environment
 import Data.Either
 
 
--- Receive [json-WKS, json-NLU] and check if the texts are the same
+-- Receive json-WKS json-NLU and check if the texts are the same
 checkTexts :: Either String WKS.Document -> Either String NLU.Document -> Bool
 checkTexts jsonWKS jsonNLU = verify jsonNLU jsonWKS
     where
@@ -36,29 +36,5 @@ reading [wksPath, nluPath] =  do
         else return False
     
 
-
--- future main:
-
--- msg =
---   " Checking for differences between WKS and NLU. \n\
---   \ Usage: \n\
---   \     NEValidation -t [json-WKS, json-NLU] (check if the texts are the same)"
-
--- help = putStrLn msg
-
--- parse ["-h"]       = help >> exitSuccess
--- parse ("-t":ls)    = checkTexts ls >> exitSuccess
--- parse ls           = help >> exitFailure
-
--- main :: IO ()
--- main = getArgs >>= parse
-
 main :: IO ()
 main = putStrLn "OK"
-
-
-
-{-
-wks <- readGJson "/home/ana/dhbb/dhbb-nlp/ner/wks/gt/59612d70-d9aa-11ea-bff2-05ea84f8fa50-40.json"
-js  <- readJSON "/home/ana/dhbb/dhbb-ner/JSON/11576.json"
--}
