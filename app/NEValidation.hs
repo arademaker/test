79a318c5a6786c40828d70b7c4274ba5ba33f39d
Ana <analuizanunes8@gmail.com>
2021-03-09 12:26:36 -0300

    add reading

--- app/NEValidation.hs
+++ app/NEValidation.hs
@@ -9,14 +9,31 @@ import Data.Either
 
 
 -- Receive [json-WKS, json-NLU] and check if the texts are the same
-checkTexts :: [FilePath] -> IO Bool
-checkTexts [gjson, json] = do
-  jsonWKS <- readGJson gjson
-  jsonNLU <- readJSON json
-  return $ verify jsonNLU jsonWKS
+checkTexts :: Either String WKS.Document -> Either String NLU.Document -> Bool
+checkTexts jsonWKS jsonNLU = verify jsonNLU jsonWKS
     where
       verify (Right x) (Right y) = analyzed_text x == docText y
       verify _ _ = False
+
+
+catchDiffs :: (WKS.Document -> [Mentions]) -> (NLU.Document -> [Entity]) -> IO Bool
+catchDiffs menNLU menWKS = return True 
+
+getMentions :: (NLU.Document -> [Entity]) -> [Mentions]
+
+
+validation :: Either String NLU.Document -> Either String WKS.Document -> IO Bool
+validation (Right jsonNLU) (Right jsonWKS) = do
+    mentionsNLU <- getMentions entities
+    return catchDiffs mentionsWKS entities 
+
+
+reading :: [FilePath] -> IO Bool
+reading [wksPath, nluPath] =  do 
+    nlu <- readJSON nluPath
+    wks <- readGJson wksPath
+    if checkTexts wks nlu then validation nlu wks 
+        else return False
     
 
 
@@ -38,3 +55,10 @@ checkTexts [gjson, json] = do
 
 main :: IO ()
 main = putStrLn "OK"
+
+
+
+{-
+wks <- readGJson "/home/ana/dhbb/dhbb-nlp/ner/wks/gt/59612d70-d9aa-11ea-bff2-05ea84f8fa50-40.json"
+js  <- readJSON "/home/ana/dhbb/dhbb-ner/JSON/11576.json"
+-}
