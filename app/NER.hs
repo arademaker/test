aa832cc66747ad938a511a19b5a77f40e63eaec4
Wellington Silva <wellington.71319@gmail.com>
2021-03-22 08:20:48 -0300

    Add structure store data in json.

--- app/NER.hs
+++ app/NER.hs
@@ -118,42 +118,36 @@ createCSV [fnNLU, fnWKS] = do
 -- Create Json
 
 data Content = Content 
-  { pessoa_pessoa :: [TypeSent]
-  , pessoa_gpe :: [TypeSent]
+  { pessoa_gpe :: [TypeSent]
   , pessoa_data :: [TypeSent]
   , pessoa_nan :: [TypeSent]
   , pessoa_lei :: [TypeSent]
   , pessoa_instituicao :: [TypeSent]
   , gpe_pessoa :: [TypeSent]
-  , gpe_gpe :: [TypeSent]
   , gpe_data :: [TypeSent]
   , gpe_nan :: [TypeSent]
   , gpe_lei :: [TypeSent]
   , gpe_instituicao :: [TypeSent]
   , data_pessoa :: [TypeSent]
   , data_gpe :: [TypeSent]
-  , data_data :: [TypeSent]
   , data_nan :: [TypeSent]
   , data_lei :: [TypeSent]
   , data_instituicao :: [TypeSent]
   , nan_pessoa :: [TypeSent]
   , nan_gpe :: [TypeSent]
   , nan_data :: [TypeSent]
-  , nan_nan :: [TypeSent]
   , nan_lei :: [TypeSent]
   , nan_instituicao :: [TypeSent]
   , lei_pessoa :: [TypeSent]
   , lei_gpe :: [TypeSent]
   , lei_data :: [TypeSent]
   , lei_nan :: [TypeSent]
-  , lei_lei :: [TypeSent]
   , lei_instituicao :: [TypeSent]
   , instituicao_pessoa :: [TypeSent]
   , instituicao_gpe :: [TypeSent]
   , instituicao_data :: [TypeSent]
   , instituicao_nan :: [TypeSent]
   , instituicao_lei :: [TypeSent]
-  , instituicao_instituicao :: [TypeSent]
   } deriving (Show, Generic)
 
 instance FromJSON Content
@@ -165,6 +159,7 @@ data TypeSent = TypeSent
     { word :: String
     , sentence :: String
     , doc :: String
+    , type_type  :: String
     } deriving (Show, Generic)
 
 instance FromJSON TypeSent
@@ -183,6 +178,118 @@ instance ToJSON Document where
 
 createJSON :: FilePath -> Document -> IO ()
 createJSON = encodeFile
+
+-- create json
+
+createDoc :: [FilePath] -> Document
+createDoc paths = 
+types = createTypeSent paths
+Document {
+  table = "..."
+  content = aux 
+    where
+      aux 
+}
+
+createTypeSent :: [FilePath] -> [TypeSent]
+createTypeSent (x:y:xs) = getSentens x y : createTypeSent xs
+createTypeSent _ = []
+
+getSentens :: FilePath -> FilePath -> [TypeSent]
+getSentens 
+nlu <- readJSON pathNLU
+wks <- readJSON pathWKS
+(Right docWKS) <- readJSON pathWKS
+text = W.docText docWKS
+fileName = W.name docWKS
+return func fileName text validation nlu wks 
+where 
+  func String -> String -> [Annotation] -> [TypeSent]
+  func name text anns = map (aux name text) anns
+    where
+      aux :: String -> String -> Annotation -> TypeSent
+      aux name text ann =
+        TypeSent {
+          doc = name
+        , word = [anBegin:anEnd] text
+        , sentence = [anBegin - 10:anEnd + 10] text
+        , type_type = anType ann
+        }
+
+
+
+    p_g = []
+  , p_d = []
+  , p_n = []
+  , p_l = []
+  , p_i = []
+  , g_p = []
+  , g_d = []
+  , g_n = []
+  , g_l = []
+  , g_i = []
+  , d_p = []
+  , d_g = []
+  , d_n = []
+  , d_l = []
+  , d_i = []
+  , n_p = []
+  , n_g = []
+  , n_d = []
+  , n_l = []
+  , n_i = []
+  , l_p = []
+  , l_g = []
+  , l_d = []
+  , l_n = []
+  , l_i = []
+  , i_p = []
+  , i_g = []
+  , i_d = []
+  , i_n = []
+  , i_l = []
+
+map aux anns 
+  where aux 
+    | anType i == "pessoa_gpe" = i ++ p_g
+    | anType i == "pessoa_data" = i ++ p_d 
+    | anType i == "pessoa_nan" = i ++ p_n 
+    | anType i == "pessoa_lei" = i ++ p_l 
+    | anType i == "pessoa_instituicao" = i ++ p_i 
+    | anType i == "gpe_pessoa" = i ++ g_p 
+    | anType i == "gpe_data" = i ++ g_d 
+    | anType i == "gpe_nan" = i ++ g_n 
+    | anType i == "gpe_lei" = i ++ g_l 
+    | anType i == "gpe_instituicao" = i ++ g_i 
+    | anType i == "data_pessoa" = i ++ d_p 
+    | anType i == "data_gpe" = i ++ d_g 
+    | anType i == "data_nan" = i ++ d_n 
+    | anType i == "data_lei" = i ++ d_l 
+    | anType i == "data_instituicao" = i ++ d_i 
+    | anType i == "nan_pessoa" = i ++ n_p 
+    | anType i == "nan_gpe" = i ++ n_g 
+    | anType i == "nan_data" = i ++ n_d 
+    | anType i == "nan_lei" = i ++ n_l 
+    | anType i == "nan_instituicao" = i ++ n_i 
+    | anType i == "lei_pessoa" = i ++ l_p 
+    | anType i == "lei_gpe" = i ++ l_g 
+    | anType i == "lei_data" = i ++ l_d 
+    | anType i == "lei_nan" = i ++ l_n 
+    | anType i == "lei_instituicao" = i ++ l_i 
+    | anType i == "instituicao_pessoa" = i ++ i_p 
+    | anType i == "instituicao_gpe" = i ++ i_g 
+    | anType i == "instituicao_data" = i ++ i_d 
+    | anType i == "instituicao_nan" = i ++ i_n 
+    | anType i == "instituicao_lei" = i ++ i_l 
+
+table = [[p_g,p_d,p_n,p_l,p_i],[g_p,g_d,g_n,g_l,g_i],[d_p,d_g,d_n,d_l,d_i],
+         [n_p,n_g,n_d,n_l,n_i],[l_p,l_g,l_d,l_n,l_i],[i_p,i_g,i_d,i_n,i_l]]
+
+
+(table !! aux head type_type) !! aux last type_type = typeSent
+
+
+
 
 -- main
 
