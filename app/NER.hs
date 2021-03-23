d2c81232f462f3c23a008998f92fd29cff9a807c
Ana <analuizanunes8@gmail.com>
2021-03-23 02:49:42 -0300

    add createDoc

--- app/NER.hs
+++ app/NER.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric  #-}
 
 
@@ -115,39 +116,38 @@ createCSV [fnNLU, fnWKS] = do
    aux (Left as) = putStrLn as
 
 
--- Create Json
 
 data Content = Content 
   { pessoa_gpe :: [TypeSent]
   , pessoa_data :: [TypeSent]
-  , pessoa_nan :: [TypeSent]
   , pessoa_lei :: [TypeSent]
   , pessoa_instituicao :: [TypeSent]
+  , pessoa_nan :: [TypeSent]
   , gpe_pessoa :: [TypeSent]
   , gpe_data :: [TypeSent]
-  , gpe_nan :: [TypeSent]
   , gpe_lei :: [TypeSent]
   , gpe_instituicao :: [TypeSent]
+  , gpe_nan :: [TypeSent]
   , data_pessoa :: [TypeSent]
   , data_gpe :: [TypeSent]
-  , data_nan :: [TypeSent]
   , data_lei :: [TypeSent]
   , data_instituicao :: [TypeSent]
+  , data_nan :: [TypeSent]
+  , lei_pessoa :: [TypeSent]
+  , lei_gpe :: [TypeSent]
+  , lei_data :: [TypeSent]
+  , lei_instituicao :: [TypeSent]
+  , lei_nan :: [TypeSent]
+  , instituicao_pessoa :: [TypeSent]
+  , instituicao_gpe :: [TypeSent]
+  , instituicao_data :: [TypeSent]
+  , instituicao_lei :: [TypeSent]
+  , instituicao_nan :: [TypeSent]
   , nan_pessoa :: [TypeSent]
   , nan_gpe :: [TypeSent]
   , nan_data :: [TypeSent]
   , nan_lei :: [TypeSent]
   , nan_instituicao :: [TypeSent]
-  , lei_pessoa :: [TypeSent]
-  , lei_gpe :: [TypeSent]
-  , lei_data :: [TypeSent]
-  , lei_nan :: [TypeSent]
-  , lei_instituicao :: [TypeSent]
-  , instituicao_pessoa :: [TypeSent]
-  , instituicao_gpe :: [TypeSent]
-  , instituicao_data :: [TypeSent]
-  , instituicao_nan :: [TypeSent]
-  , instituicao_lei :: [TypeSent]
   } deriving (Show, Generic)
 
 instance FromJSON Content
@@ -159,7 +159,7 @@ data TypeSent = TypeSent
     { word :: String
     , sentence :: String
     , doc :: String
-    , type_type  :: String
+    , typeType  :: String
     } deriving (Show, Generic)
 
 instance FromJSON TypeSent
@@ -180,113 +180,157 @@ createJSON :: FilePath -> Document -> IO ()
 createJSON = encodeFile
 
 -- create json
+createDoc :: [FilePath] -> FilePath -> IO()
+createDoc as path = createJSON path (createTable . createTS as)
 
-createDoc :: [FilePath] -> Document
-createDoc paths = 
-types = createTypeSent paths
-Document {
-  table = "..."
-  content = aux 
-    where
-      aux 
-}
 
-createTypeSent :: [FilePath] -> [TypeSent]
-createTypeSent (x:y:xs) = getSentens x y : createTypeSent xs
-createTypeSent _ = []
+createTS :: [FilePath] -> [TypeSent]
+createTS (x:y:xs) = getSentens x y : createTS xs
+createTS _ = [] 
+  
 
 getSentens :: FilePath -> FilePath -> [TypeSent]
-getSentens 
-nlu <- readJSON pathNLU
-wks <- readJSON pathWKS
-(Right docWKS) <- readJSON pathWKS
-text = W.docText docWKS
-fileName = W.name docWKS
-return func fileName text validation nlu wks 
-where 
-  func String -> String -> [Annotation] -> [TypeSent]
-  func name text anns = map (aux name text) anns
-    where
-      aux :: String -> String -> Annotation -> TypeSent
-      aux name text ann =
-        TypeSent {
-          doc = name
-        , word = [anBegin:anEnd] text
-        , sentence = [anBegin - 10:anEnd + 10] text
-        , type_type = anType ann
-        }
+getSentens pathNLU pathWKS = do
+  nlu <- N.readJSON pathNLU
+  wks <- W.readJSON pathWKS
+  (Right docWKS) <- W.readJSON pathWKS
+  text <- W.docText docWKS
+  fileName <- W.name docWKS
+  return (func fileName text validation nlu wks) 
+  where 
+    func :: String -> String -> [Annotation] -> [TypeSent]
+    func name text anns = concatMap (aux name text) anns
+      where
+        aux :: String -> String -> Annotation -> TypeSent
+        aux name text ann =
+          TypeSent {
+            doc = name
+          , word = (take (anEnd - anBegin)) . (drop anBegin) text
+          , sentence = (take (10 + anEnd - anBegin)) . (drop (anBegin - 10 )) text
+          , typeType = intercalate "_" anType ann                  
+          }
 
 
+createTable :: [TypeSent] -> Document
+createTable as
+  let   p_p = [] 
+        p_g = [] 
+        p_d = []
+        p_n = []
+        p_l = []
+        p_i = []
+        g_p = []
+        g_g = []
+        g_d = []
+        g_n = []
+        g_l = []
+        g_i = []
+        d_p = []
+        d_g = []
+        d_d = []
+        d_n = []
+        d_l = []
+        d_i = []
+        l_p = []
+        l_g = []
+        l_d = []
+        l_n = []
+        l_l = []
+        l_i = []
+        i_p = []
+        i_g = []
+        i_d = []
+        i_n = []
+        i_l = []
+        i_i = []
+        n_p = []
+        n_g = []
+        n_d = []
+        n_l = []
+        n_i = []
+  in
+   map aux as 
+      where 
+        aux a 
+          | typeType a == "pessoa_pessoa" = a ++ p_p
+          | typeType a == "pessoa_gpe" = a ++ p_g
+          | typeType a == "pessoa_data" = a ++ p_d 
+          | typeType a == "pessoa_lei" = a ++ p_l 
+          | typeType a == "pessoa_instituicao" = a ++ p_i 
+          | typeType a == "pessoa_nan" = a ++ p_n 
+          | typeType a == "gpe_pessoa" = a ++ g_p 
+          | typeType a == "gpe_gpe" = a ++ g_g
+          | typeType a == "gpe_data" = a ++ g_d 
+          | typeType a == "gpe_lei" = a ++ g_l 
+          | typeType a == "gpe_instituicao" = a ++ g_i 
+          | typeType a == "gpe_nan" = a ++ g_n 
+          | typeType a == "data_pessoa" = a ++ d_p 
+          | typeType a == "data_gpe" = a ++ d_g 
+          | typeType a == "data_data" = a ++ d_d
+          | typeType a == "data_lei" = a ++ d_l 
+          | typeType a == "data_instituicao" = a ++ d_i 
+          | typeType a == "data_nan" = a ++ d_n 
+          | typeType a == "lei_pessoa" = a ++ l_p 
+          | typeType a == "lei_gpe" = a ++ l_g 
+          | typeType a == "lei_data" = a ++ l_d 
+          | typeType a == "lei_lei" = a ++ l_l
+          | typeType a == "lei_instituicao" = a ++ l_i 
+          | typeType a == "lei_nan" = a ++ l_n 
+          | typeType a == "instituicao_pessoa" = a ++ i_p 
+          | typeType a == "instituicao_gpe" = a ++ i_g 
+          | typeType a == "instituicao_data" = a ++ i_d 
+          | typeType a == "instituicao_lei" = a ++ i_l 
+          | typeType a == "instituicao_instituicao" = a ++ i_i
+          | typeType a == "instituicao_nan" = a ++ i_n 
+          | typeType a == "nan_pessoa" = a ++ n_p 
+          | typeType a == "nan_gpe" = a ++ n_g 
+          | typeType a == "nan_data" = a ++ n_d 
+          | typeType a == "nan_lei" = a ++ n_l 
+          | typeType a == "nan_instituicao" = a ++ n_i 
 
-    p_g = []
-  , p_d = []
-  , p_n = []
-  , p_l = []
-  , p_i = []
-  , g_p = []
-  , g_d = []
-  , g_n = []
-  , g_l = []
-  , g_i = []
-  , d_p = []
-  , d_g = []
-  , d_n = []
-  , d_l = []
-  , d_i = []
-  , n_p = []
-  , n_g = []
-  , n_d = []
-  , n_l = []
-  , n_i = []
-  , l_p = []
-  , l_g = []
-  , l_d = []
-  , l_n = []
-  , l_i = []
-  , i_p = []
-  , i_g = []
-  , i_d = []
-  , i_n = []
-  , i_l = []
+   tab = [[length p_p, length p_g, length p_d, length p_l, length p_i, length p_n],
+          [length g_p, length g_g, length g_d, length g_l, length g_i, length g_n],
+          [length d_p, length d_g, length d_d, length d_l, length d_i, length d_n],
+          [length l_p, length l_g, length l_d, length l_l, length l_i, length l_n],
+          [length i_p, length i_g, length i_d, length i_l, length i_i, length i_n],
+          [length n_p, length n_g, length n_d, length n_l, length n_i, 0 ]]
 
-map aux anns 
-  where aux 
-    | anType i == "pessoa_gpe" = i ++ p_g
-    | anType i == "pessoa_data" = i ++ p_d 
-    | anType i == "pessoa_nan" = i ++ p_n 
-    | anType i == "pessoa_lei" = i ++ p_l 
-    | anType i == "pessoa_instituicao" = i ++ p_i 
-    | anType i == "gpe_pessoa" = i ++ g_p 
-    | anType i == "gpe_data" = i ++ g_d 
-    | anType i == "gpe_nan" = i ++ g_n 
-    | anType i == "gpe_lei" = i ++ g_l 
-    | anType i == "gpe_instituicao" = i ++ g_i 
-    | anType i == "data_pessoa" = i ++ d_p 
-    | anType i == "data_gpe" = i ++ d_g 
-    | anType i == "data_nan" = i ++ d_n 
-    | anType i == "data_lei" = i ++ d_l 
-    | anType i == "data_instituicao" = i ++ d_i 
-    | anType i == "nan_pessoa" = i ++ n_p 
-    | anType i == "nan_gpe" = i ++ n_g 
-    | anType i == "nan_data" = i ++ n_d 
-    | anType i == "nan_lei" = i ++ n_l 
-    | anType i == "nan_instituicao" = i ++ n_i 
-    | anType i == "lei_pessoa" = i ++ l_p 
-    | anType i == "lei_gpe" = i ++ l_g 
-    | anType i == "lei_data" = i ++ l_d 
-    | anType i == "lei_nan" = i ++ l_n 
-    | anType i == "lei_instituicao" = i ++ l_i 
-    | anType i == "instituicao_pessoa" = i ++ i_p 
-    | anType i == "instituicao_gpe" = i ++ i_g 
-    | anType i == "instituicao_data" = i ++ i_d 
-    | anType i == "instituicao_nan" = i ++ i_n 
-    | anType i == "instituicao_lei" = i ++ i_l 
+   cont = Content {
+       pessoa_gpe = p_g
+     , pessoa_data = p_d
+     , pessoa_nan = p_n
+     , pessoa_lei = p_l
+     , pessoa_instituicao = p_i
+     , gpe_pessoa = g_p
+     , gpe_data = g_d
+     , gpe_nan = g_n
+     , gpe_lei = g_l
+     , gpe_instituicao = g_i
+     , data_pessoa = d_p
+     , data_gpe = d_g
+     , data_nan = d_n
+     , data_lei = d_l
+     , data_instituicao = d_i
+     , nan_pessoa = n_p
+     , nan_gpe = n_g
+     , nan_data = n_d
+     , nan_lei = n_l
+     , nan_instituicao = n_i
+     , lei_pessoa = l_p
+     , lei_gpe = l_g
+     , lei_data = l_d
+     , lei_nan = l_n
+     , lei_instituicao = l_i
+     , instituicao_pessoa = i_p
+     , instituicao_gpe = i_g
+     , instituicao_data = i_d
+     , instituicao_nan = i_n
+     , instituicao_lei = i_l
+    }
 
-table = [[p_g,p_d,p_n,p_l,p_i],[g_p,g_d,g_n,g_l,g_i],[d_p,d_g,d_n,d_l,d_i],
-         [n_p,n_g,n_d,n_l,n_i],[l_p,l_g,l_d,l_n,l_i],[i_p,i_g,i_d,i_n,i_l]]
+   return (Document { table = tab, content = cont })
 
 
-(table !! aux head type_type) !! aux last type_type = typeSent
 
 
 
