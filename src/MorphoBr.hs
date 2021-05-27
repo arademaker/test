2ed00f8882da58a074718d495545e4673595f4f3
Ana <analuizanunes8@gmail.com>
2021-05-27 11:26:32 -0300

    corrected and documented

--- src/MorphoBr.hs
+++ src/MorphoBr.hs
@@ -12,6 +12,12 @@ import qualified Data.Text.IO as TO
 import Data.List (groupBy, intercalate, sort)
 import Data.Maybe ( fromJust, isNothing )
 
+
+---- Criação e simplificação do map
+
+-- Na simplificação, para cada chave, os valores são ordenados e agrupados de acordo com o lema,
+-- em seguida é feita a interseção dois a dois de cada grupo 
+
 member :: T.Text  -> [T.Text] -> Bool
 member x [] = False
 member x (y:ys) | x==y = True
@@ -40,15 +46,10 @@ readF1 fn = do
   content <- TO.readFile fn
   return $ M.fromListWith (++) $ lines2pairs (T.lines content)
 
-check :: M.Map T.Text [T.Text] -> [T.Text] -> T.Text
-check m xs
- | member (last xs) (fromJust (M.lookup (head xs) m)) = ""
- | otherwise = T.append (last xs) $ T.append " | " (T.intercalate " " (fromJust (M.lookup (head xs) m)))
-
 createMap :: FilePath -> [FilePath] -> IO (M.Map T.Text [T.Text])
 createMap dir paths = do
   dicts <- mapM (readF1 . combine dir) paths
-  return (fix $ M.map simplify $ foldr M.union M.empty dicts)
+  return (M.map simplify $ foldr M.union M.empty dicts)
 
 clean :: [T.Text] -> [T.Text]
 clean (x:xs)
@@ -56,6 +57,15 @@ clean (x:xs)
  | otherwise = x : clean xs
 clean [] = []
 
+---- Comparação de resultados
+
+check :: M.Map T.Text [T.Text] -> [T.Text] -> T.Text
+check m xs
+ | member (last xs) (fromJust (M.lookup (head xs) m)) = ""
+ | otherwise = T.append (last xs) $ T.append " | " (T.intercalate " " (fromJust (M.lookup (head xs) m)))
+
+-- recebe o diretório noun ou o diretório adjectives do MorphoBr e um diretório contendo os
+-- arquivos produzidos pelo script em python
 getDiffs :: FilePath -> FilePath -> IO [[T.Text]]
 getDiffs mpath epath = do
   mfiles <- listDirectory mpath
@@ -67,27 +77,58 @@ getDiffs mpath epath = do
        content <- TO.readFile $ combine dir path
        return (clean $ map (check m . (T.splitOn "\t")) (T.lines content))
 
+---- Correção de erros
+
+-- lebrão e lebrões foram simplificados mas não deveriam ser
+-- arquivo nouns-ac.dict
 erro1 :: M.Map T.Text [T.Text] -> M.Map T.Text [T.Text]
 erro1 m =
   M.insert (T.pack "lebrões") [(T.pack "lebre+N+AUG+M+PL"), (T.pack "lebre+N+M+PL")] $ 
   M.insert (T.pack "lebrão") [(T.pack "lebre+N+AUG+M+SG"),(T.pack "lebre+N+M+SG")] m
 
+-- a simplificação de "zurupável" estava errada, o motivo desse erro foi a duplicação de 
+-- "zurupável	zurupável+A+M+SG" no arquivo adjectives-ae.dict
 erro2 :: M.Map T.Text [T.Text] -> M.Map T.Text [T.Text]
-erro2 m = 
-  M.insert (T.pack "zurupável") [T.pack "zurupável+A+SG"] m
+erro2 = 
+  M.insert (T.pack "zurupável") [T.pack "zurupável+A+SG"]
 
+
+-- "cimbráveis" não era simplificado porque ao unir os maps de cada arquivo do diretório adjectives
+-- o valor da chave "cimbráveis" (cimbrável+A+F+PL), produzido pelo map do arquivo adjectives-aa.dict, 
+-- era substituído  pelo valor (cimbrável+A+M+PL) produzido pelo map do arquivo adjectives-ab.dict
 erro3 :: M.Map T.Text [T.Text] -> M.Map T.Text [T.Text]
-erro3 m =
-  M.insert (T.pack "cimbráveis") [T.pack "cimbrável+A+PL"] m
+erro3 =
+  M.insert (T.pack "cimbráveis") [T.pack "cimbrável+A+PL"]
 
-fix :: M.Map T.Text [T.Text] -> M.Map T.Text [T.Text]
-fix m = erro1 $ erro2 $ erro3 m 
+
+-- fixN e fixA são usadas para corrigir erros pontuais, isso é feito inserindo as entradas corretas
+-- Obs: ao inserir uma nova chave, se já existir uma chave igual no map, os valores da
+-- chave antiga serão substituídos pelos valores da chave nova
+fixA :: M.Map T.Text [T.Text] -> M.Map T.Text [T.Text]
+fixA m = erro2 $ erro3 m 
+
+fixN :: M.Map T.Text [T.Text] -> M.Map T.Text [T.Text]
+fixN m = erro1 m
+
+
+---- Produção de novos arquivos 
 
 serialize :: (T.Text, [T.Text]) -> [T.Text] 
 serialize (k,xs) = map (\x -> T.append k (T.append "\t" x)) xs
 
-newMorpho :: FilePath -> FilePath -> IO ()
-newMorpho path outpath = do
+-- newADJ recebe o diretório adjectives do MorphoBr e um path
+-- onde será salva a versão compacta
+newADJ :: FilePath -> FilePath -> IO ()
+newADJ path outpath = do
   files <- listDirectory path
   m <- createMap path files
-  TO.writeFile outpath (T.intercalate "\n" $ concatMap serialize (M.toList m))
+  TO.writeFile outpath (T.intercalate "\n" $ concatMap serialize (M.toList (fixA m)))
+
+-- newNouns recebe o diretório nouns do MorphoBr e um path
+-- onde será salva a versão compacta
+newNouns :: FilePath -> FilePath -> IO ()
+newNouns path outpath = do
+  files <- listDirectory path
+  m <- createMap path files
+  TO.writeFile outpath (T.intercalate "\n" $ concatMap serialize (M.toList (fixN m)))
+
